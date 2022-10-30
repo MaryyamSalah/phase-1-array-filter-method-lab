a2efb32301a6011ef89484fa2e6720289737bca4
MaryyamSalah <maryyamslah20@gmail.com>
2022-10-30 15:50:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+
+function findMatching(drivers , match){
+   const newArray = drivers.filter(function(name){
+        return name.toLowerCase() === match.toLowerCase()
+    })
+return newArray
+}
+
+function fuzzyMatch(drivers , match){
+    const newArray = drivers.filter(function(name){
+        return name.charAt(0) === match.charAt(0)
+    })
+return newArray
+}
+
+function matchName(drivers , match){
+    const newArray = drivers.filter(function(name){
+        return name.name === match
+    })
+return newArray
+}
